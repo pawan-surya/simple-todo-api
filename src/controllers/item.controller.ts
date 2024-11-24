250dcc10c6f8ba5f66f9c19982c61ad74975b6c2
pawansuryaji <66516775+pawan-surya@users.noreply.github.com>
2024-11-24 20:35:50 +0530

    Created authentication controller, routes, and methods for getting, updating, and deleting todo items, along with validations and authentication

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -4,14 +4,34 @@ import moment from 'moment';
 
 export const getItems = async (req: Request, res: Response) => {
   try {
-    const todos = await Item.find({ user: req.user._id });
+    const todos = await Item.aggregate([
+      { $match: { user: req.user._id } },
+      {
+        $project: {
+          _id: 1,
+          title: 1, 
+          description: 1,
+          completed: 1,
+          dueDate: {
+            $dateToString: {
+              format: "%Y-%m-%d",
+              date: "$dueDate",
+              timezone: "UTC"
+            }
+          }
+        }
+      },
+      { $sort: { dueDate: 1 } }
+    ])
+    
     res.json(todos);
   } catch (error) {
+    console.log("error", error)
     res.status(500).json({ error: 'Error fetching todo-items' });
   }
 };
 
-
+// For Now allowing Old dates while create but we can validate and stop the old date todos creation
 export const generateTodo = async (req: Request, res: Response) => {
   try {
     const todo = new Item({...req.body,user: req.user._id});
@@ -28,5 +48,65 @@ export const generateTodo = async (req: Request, res: Response) => {
     res.status(201).json(req.body);
   } catch (error) {
     res.status(400).json({ error: 'Error creating todo-item' });
+  }
+};
+
+
+export const getItem = async (req: Request, res: Response) => {
+  try {
+    const todo = await Item.findOne({ _id: req.params.id, user: req.user._id });
+
+
+    if (!todo) {
+       res.status(404).json({ error: 'Item not found' });
+       return;
+    }
+
+    const formattedTodo = {
+      ...todo.toObject(),
+      dueDate: moment(todo.dueDate).format('MMMM DD, YYYY'),
+    };
+
+    res.status(200).json(formattedTodo);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching todo' });
+  }
+};
+
+export const deleteItem = async (req: Request, res: Response) => {
+  try {
+    const todo = await Item.findOneAndDelete({ _id: req.params.id, user: req.user._id });
+    if (!todo) {
+      res.status(404).json({ error: 'Item not found' });
+      return;
+    }
+    res.json({ message: 'Todo Item deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting todo item.' });
+  }
+}; 
+
+export const updateItem = async (req: Request, res: Response) => {
+  try {
+    if (req.body.dueDate) {
+      const dueDate = new Date(req.body.dueDate);
+      if (dueDate < new Date()) {
+        res.status(400).json({ error: 'Due date cannot be in the past' });
+        return;
+      }
+    }
+
+    const todo = await Item.findOneAndUpdate(
+      { _id: req.params.id, user: req.user._id },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!todo) {
+       res.status(404).json({ error: 'Todo item not found' });
+       return;
+    }
+    res.json(todo);
+  } catch (error) {
+    res.status(400).json({ error: 'Error updating todo item' });
   }
 };
--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import { auth } from '../middleware/auth.middleware';
 import { validateRequest } from '../middleware/validate.middleware';
 import { todoValidator } from '../validators/item.validator';
-import {generateTodo,getItems} from '../controllers/item.controller';
+import {generateTodo,getItems,getItem,deleteItem,updateItem} from '../controllers/item.controller';
 
 const router = express.Router();
 router.use(auth); 
 
 router.get('/lists',getItems);
 router.post('/store', todoValidator, validateRequest, generateTodo);
-
+router.get('/:id', getItem);
+router.put('/:id', todoValidator, validateRequest, updateItem);
+router.delete('/:id', deleteItem);
 
 export default router; 
